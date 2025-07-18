75a21651f089fc4827cb8f78f821c217fd77dcd3
KrissanVan <krissanvandelacruz@gmail.com>
2025-07-18 23:56:43 +0800

    Added Popup page for about

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,10 @@ document.getElementById("user-profile").addEventListener("click", function(){
   window.location.href = "/src/pages/profile.html";
 });
 
+function aboutOpenButton(){
+  document.getElementById("about-popup").style.display = "Block"; 
+}
+function aboutCloseButton(){
+  document.getElementById("about-popup").style.display = "none";
+}
+
